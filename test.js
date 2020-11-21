1e8ace3cdd7d259adc1abb074e9adeaa00104b6d
Maggie Kasunic <maggie.kasunic@gmail.com>
2020-11-20 18:57:27 -0600

    jest and enzyme setup

--- test.js
+++ test.js
@@ -2,6 +2,20 @@ const app = require('./server/index.js') // Link to your server file
 const supertest = require('supertest')
 const request = supertest(app)
 
+var enzyme = require('enzyme');
+
+import regeneratorRuntime from "regenerator-runtime";
+
+import Title from './client/src/components/Title';
+
+describe('Title', () => {
+  it('should render correctly in "debug" mode', () => {
+    const component = shallow(<Title debug />);
+
+    expect(component).toMatchSnapshot();
+  });
+});
+
 it("Testing to see if Jest works", () => {
   expect(1).toBe(1);
 });
