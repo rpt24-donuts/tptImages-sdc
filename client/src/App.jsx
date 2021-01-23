4aba394ec9199993341390009eb77ca1f025b606
Tina-Luk <tina.luk.tina@gmail.com>
2021-01-23 13:39:27 -0800

    set up front end config

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,73 +6,75 @@ import MainImage from './components/MainImage.jsx';
 import Info from './components/Info.jsx';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      id: '15',
-      mainImage:'',
-      imagesList: [],
-      title: 'Title Placeholder',
-      average: 0,
-      ratings: 0,
-      pages: 0,
-      standards:['RL 4.4', 'RL 4.5'],
-      grades: '4th Grade, 5th Grade'
-    }
-    this.onHover = this.onHover.bind(this);
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			id: '15',
+			mainImage: '',
+			imagesList: [],
+			title: 'Title Placeholder',
+			average: 0,
+			ratings: 0,
+			pages: 0,
+			standards: ['RL 4.4', 'RL 4.5'],
+			grades: '4th Grade, 5th Grade',
+		};
+		this.onHover = this.onHover.bind(this);
+	}
 
-  onHover(url) {
-    this.setState({
-      mainImage: url
-    });
-  }
+	onHover(url) {
+		this.setState({
+			mainImage: url,
+		});
+	}
 
-  componentDidMount() {
-    let id = window.location.href.split("/")[4];
-    this.setState({
-      id: id
-    });
-    $.get(`http://localhost:3003/${id}/images`, (data) => {
-        this.setState({
-          mainImage: data[id][0],
-          imagesList: data[id]
-        });
-    });
-    $.get(`http://18.144.61.129:3001/products/${id}/ratings`, (data) => {
-      let gradeList = [];
-      data[2].forEach(grade => {
-        gradeList.push(grade._id)
-      });
-      gradeList=gradeList.join(', ');
-      this.setState({
-        average: data[1][0].average,
-        ratings: data[1][0].count,
-        title: data[0],
-        grades: gradeList
-      });
-    });
-    $.get(`http://18.222.237.222:3002/products/${id}/description-and-standards`, (data) => {
-      let standards = [];
-      for (var key in data.standards) {
-        standards.push(key)
-      }
-      this.setState({
-        pages: data.pageLength,
-        standards: standards
-      });
-    });
-  }
+	componentDidMount() {
+		// example browse url http://localhost:3003/?products/4
+		let id = window.location.href.split('/')[4];
+		this.setState({
+			id: id,
+		});
+		$.get(`http://localhost:3003/${id}/images`, (data) => {
+			console.log(data);
+			this.setState({
+				mainImage: data[id][0],
+				imagesList: data[id],
+			});
+		});
+		$.get(`http://localhost:3001/products/${id}/ratings`, (data) => {
+			let gradeList = [];
+			data[2].forEach((grade) => {
+				gradeList.push(grade._id);
+			});
+			gradeList = gradeList.join(', ');
+			this.setState({
+				average: data[1][0].average,
+				ratings: data[1][0].count,
+				title: data[0],
+				grades: gradeList,
+			});
+		});
+		$.get(`http://localhost:3002/products/${id}/description-and-standards`, (data) => {
+			let standards = [];
+			for (var key in data.standards) {
+				standards.push(key);
+			}
+			this.setState({
+				pages: data.pageLength,
+				standards: standards,
+			});
+		});
+	}
 
-  render() {
-    return (
-      <div id="images-app">
-        <Title title={this.state.title} average={this.state.average} ratings={this.state.ratings}/>
-        <MainImage image={this.state.mainImage} list={this.state.imagesList} hover={this.onHover}/>
-        <Info pages={this.state.pages} standards={this.state.standards} grades={this.state.grades}/>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div id="images-app">
+				<Title title={this.state.title} average={this.state.average} ratings={this.state.ratings} />
+				<MainImage image={this.state.mainImage} list={this.state.imagesList} hover={this.onHover} />
+				<Info pages={this.state.pages} standards={this.state.standards} grades={this.state.grades} />
+			</div>
+		);
+	}
 }
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -3,36 +3,36 @@ var S3Plugin = require('webpack-s3-plugin');
 const aws = require('./aws_config.js');
 
 module.exports = {
-  entry: path.join(__dirname, '/client/src/index.jsx'),
-  output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, '/client/dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: [/\.jsx?/],
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react', '@babel/preset-env']
-          },
-        },
-      },
-    ],
-  },
-  plugins: [
-    new S3Plugin({
-      include:'bundle.js',
-      s3Options: {
-        accessKeyId: aws.aws_access_key_id,
-        secretAccessKey: aws.aws_secret_access_key
-      },
-      s3UploadOptions: {
-        Bucket: 'fec-proxy2020'
-      }
-    })
-  ],
-  watch: true
+	entry: path.join(__dirname, '/client/src/index.jsx'),
+	output: {
+		filename: 'bundle.js',
+		path: path.join(__dirname, '/client/dist'),
+	},
+	module: {
+		rules: [
+			{
+				test: [/\.jsx?/],
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-react', '@babel/preset-env'],
+					},
+				},
+			},
+		],
+	},
+	plugins: [
+		new S3Plugin({
+			include: 'bundle.js',
+			s3Options: {
+				accessKeyId: aws.aws_access_key_id,
+				secretAccessKey: aws.aws_secret_access_key,
+			},
+			s3UploadOptions: {
+				Bucket: 'tpt-imagesmodule-sdc',
+			},
+		}),
+	],
+	watch: true,
 };
