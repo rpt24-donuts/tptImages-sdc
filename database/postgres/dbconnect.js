d88db0f7d22d27c920c284aaa4cd56f293bb1751
Tina-Luk <tina.luk.tina@gmail.com>
2021-02-06 15:21:44 -0800

    Fix data generation script to drain

--- database/postgres/dbconnect.js
+++ database/postgres/dbconnect.js
@@ -1,22 +1,10 @@
-const { Pool, Client } = require('pg');
+const { Pool } = require('pg');
 const pool = new Pool({
 	user: 'postgres',
 	database: 'sdcimages',
 	password: 'password',
 });
-pool.query("COPY images(images, item) FROM '/home/chris/HackReactor/SDC/tptImpages-sdc/database/data.csv' DELIMITER ',' CSV HEADER;", (err, res) => {
+pool.query("COPY images(item, images) FROM '/home/chris/HackReactor/SDC/tptImpages-sdc/database/data.csv' DELIMITER ',' CSV HEADER;", (err, res) => {
 	console.log(err, res);
 	pool.end();
 });
-// const client = new Client({
-// 	user: 'dbuser',
-// 	host: 'database.server.com',
-// 	database: 'mydb',
-// 	password: 'secretpassword',
-// 	port: 3211,
-// });
-// client.connect();
-// client.query('SELECT NOW()', (err, res) => {
-// 	console.log(err, res);
-// 	client.end();
-// });
